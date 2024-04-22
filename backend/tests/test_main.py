9ddda369643029678afb0ac6a045fa36f0a54213
Syed Kamil Zafar <kamilzafar54@gmail.com>
2024-04-22 16:55:11 +0500

    Add more tests

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -4,68 +4,77 @@ from ecom.utils.db import db_session
 from ecom.utils import settings
 from sqlmodel import create_engine, Session, SQLModel
 
-# def test_create_user():
-#     client = TestClient(app)
+def test_read_main():
+    client = TestClient(app)
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"Hello": "World"}
 
-#     connection_string = str(settings.TEST_DATABASE_URL)
+def test_create_user():
 
-#     engine = create_engine(
-#         connection_string, connect_args={"sslmode": "require"}, pool_recycle=300)
+    connection_string = str(settings.TEST_DATABASE_URL)
 
-#     SQLModel.metadata.create_all(engine)  
+    engine = create_engine(
+        connection_string, connect_args={"sslmode": "require"}, pool_recycle=300)
 
-#     with Session(engine) as session:  
+    SQLModel.metadata.create_all(engine)  
 
-#         def get_session_override():  
-#                 yield session  
+    with Session(engine) as session:  
 
-#         app.dependency_overrides[db_session] = get_session_override 
+        def get_session_override():  
+                yield session  
 
-#         response = client.post(
-#             "/api/signup",
-#             json={
-#                 "username": "testuser1",
-#                 "email": "testuser1@gmail.com",
-#                 "password": "password"
-#             }
-#         )
-#         assert response.status_code == 200
-#         assert response.json()["username"] == "testuser1"
+        app.dependency_overrides[db_session] = get_session_override 
 
-# def test_login_for_access_token():
-#     client = TestClient(app)
+        client = TestClient(app=app)
 
-#     connection_string = str(settings.TEST_TEST_DATABASE_URL)
+        response = client.post(
+            "/api/signup",
+            json={
+                "username": "testuser1",
+                "email": "testuser1@gmail.com",
+                "password": "password"
+            }
+        )
+        assert response.status_code == 200
+        assert response.json()["username"] == "testuser1"
 
-#     engine = create_engine(
-#         connection_string, connect_args={"sslmode": "require"}, pool_recycle=300
-#     )
+def test_login_for_access_token():
 
-#     SQLModel.metadata.create_all(engine)
+    connection_string = str(settings.TEST_DATABASE_URL)
 
-#     with Session(engine) as session:
+    engine = create_engine(
+        connection_string, connect_args={"sslmode": "require"}, pool_recycle=300
+    )
 
-#         def get_session_override():
-#             yield session
+    SQLModel.metadata.create_all(engine)
 
-#         app.dependency_overrides[db_session] = get_session_override
+    with Session(engine) as session:
 
-#         response = client.post(
-#             "/api/login",
-#             data={
-#                 "username": "kamil",
-#                 "password": "kamil"
-#             }
-#         )
+        def get_session_override():
+            yield session
 
-#         assert response.status_code == 200
-#         assert "access_token" in response.json()
-#         assert "refresh_token" in response.json()
-#         assert "expires_in" in response.json()
-#         assert "token_type" in response.json()
+        client = TestClient(app=app)
+
+        app.dependency_overrides[db_session] = get_session_override
+
+        response = client.post(
+            "/api/login",
+            data={
+                "username": "testuser1",
+                "password": "password"
+            }
+        )
+
+        assert response.status_code == 200
+        assert "access_token" in response.json()
+        assert "refresh_token" in response.json()
+        assert "expires_in" in response.json()
+        assert "token_type" in response.json()
+
 
 def test_get_product():
-    client = TestClient(app=app)
+
     connection_string = str(settings.TEST_DATABASE_URL)
 
     engine = create_engine(
@@ -80,12 +89,13 @@ def test_get_product():
 
         app.dependency_overrides[db_session] = get_session_override 
 
+        client = TestClient(app=app)
+
         response = client.get("/api/products")
         assert response.status_code == 200
         assert response.json()[0]["name"] == "SLOGAN PRINT T-SHIRT"
 
 def test_get_product_by_slug():
-    client = TestClient(app)
 
     connection_string = str(settings.TEST_DATABASE_URL)
 
@@ -102,6 +112,8 @@ def test_get_product_by_slug():
 
         app.dependency_overrides[db_session] = get_session_override
 
+        client = TestClient(app=app)
+
         # Assuming a product with slug "test-product" exists in the database
         response = client.get("/api/products/slogan-print-t-shirt")
 
@@ -110,4 +122,6 @@ def test_get_product_by_slug():
         assert "description" in response.json()
         assert "price" in response.json()
         assert "slug" in response.json()
+        assert "image1" in response.json()
+        assert "image2" in response.json()
         assert response.json()["slug"] == "slogan-print-t-shirt"
