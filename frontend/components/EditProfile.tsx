a100be33bb0108428a78853a5b8e5d3b28a2df07
Syed Kamil Zafar <kamilzafar54@gmail.com>
2024-04-23 07:32:14 +0500

    Add useToast hook and update submit function in EditProfile.tsx

--- frontend/components/EditProfile.tsx
+++ frontend/components/EditProfile.tsx
@@ -13,6 +13,7 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { useState } from "react"
+import { useToast } from "./ui/use-toast"
 
 type User = {
   name: string;
@@ -21,9 +22,13 @@ type User = {
 
 export function DialogDemo({name, email}: User) {
   let [username, setUsername] = useState(name) 
-  const submit = async (e) => {
-    e.preventDefault()
+  const {toast} = useToast()
+  const submit = async () => {
     await updateUser(username)
+    toast({
+      title: "Profile updated",
+      description: "Your profile has been updated successfully",
+    })
   }
   return (
     <Dialog>
