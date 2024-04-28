1f195c7ca93f46d0c65c382b84d53bb8cb648cef
Syed Kamil Zafar <kamilzafar54@gmail.com>
2024-04-29 00:56:05 +0500

    Refactor auth.ts to fix cookie handling and add missing functions

--- frontend/lib/auth.ts
+++ frontend/lib/auth.ts
@@ -1,22 +1,6 @@
 "use server";
 import { cookies } from 'next/headers';
 
-export const myDeleteCookie = () => {
-    cookies().delete("access_token") 
-    cookies().delete("refresh_token")
-    console.log("cookie deleted");
-}
-
-export const myGetCookie = () => {
-    const tokens = cookies().get("access_token")?.value || cookies().get("refresh_token")?.value
-    return tokens
-}
-
-export const checkCookie = () => {
-    const cookie = cookies().get("access_token") || cookies().get("refresh_token")
-    return cookie
-}
-
 export const mySetCookie = (access_token: string, refresh_token: string) => {
     cookies().set({
         name: 'access_token',
@@ -33,6 +17,22 @@ export const mySetCookie = (access_token: string, refresh_token: string) => {
         expires: new Date(Date.now() + 60 * 60 * 1000 * 24 * 7)
     })
     console.log("cookie set");
+}
+
+export const myDeleteCookie = () => {
+    cookies().delete("access_token") 
+    cookies().delete("refresh_token")
+    console.log("cookie deleted");
+}
+
+export const myGetCookie = () => {
+    const tokens = cookies().get("access_token")?.value || cookies().get("refresh_token")?.value
+    return tokens
+}
+
+export const checkCookie = () => {
+    const cookie = cookies().has("access_token") || cookies().has("refresh_token")
+    return cookie
 }
 
 export const setThreadID = (thread_id: string) => {
